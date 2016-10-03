d64b1b3da1d00c228def1bda297e7a9df20b9da4
Tom Browder <tom.browder@gmail.com>
2016-10-03 09:55:55 -0500

    add TODO item; add debug to shell script

--- install-zef.sh
+++ install-zef.sh
@@ -6,7 +6,8 @@ git clone https://github.com/ugexe/zef.git
 cd ./zef
 
 # the following two lines are for added info in the travis build log"
-TD=`pwd`
-echo "=== now working in dir '$TD' ==="
+D=`pwd`
+echo "=== now working in dir '$D' ==="
+echo "=== current path: '$PATH' ==="
 # the coup de grace:
 perl6 -Ilib bin/zef install .
